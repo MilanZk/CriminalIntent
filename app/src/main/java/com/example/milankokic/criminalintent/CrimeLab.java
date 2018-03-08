18695d94d8bb22d4fcffb959edb364afdc7bcef2
MKokic <mkokicm@gmail.com>
2018-03-08 15:26:35 +0100

    Added CrimeListActivity

---
+++ app/src/main/java/com/example/milankokic/criminalintent/CrimeLab.java
@@ -0,0 +1,43 @@
+package com.example.milankokic.criminalintent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.naming.Context;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    public List<Crime> mCrimes;
+
+    public CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Every other one
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/example/milankokic/criminalintent/CrimeListActivity.java
@@ -0,0 +1,6 @@
+package com.example.milankokic.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CrimeListActivity  extends AppCompatActivity{
+}
